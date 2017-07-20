b4f07d86509e97b86f9e03c1c4f89a45da02d367
tsuyoshi YASUMATSU <yasumag@gmail.com>
2017-07-20 14:06:18 +0000

    second commit

--- test-carousel.js
+++ test-carousel.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: true,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
 });
